69ac2902e7bee297685dc419714f74cf0513bcd2
Mherab Ahmed <P2598656@my365.dmu.ac.uk>
2022-04-26 16:16:31 +0100

    added test methods for each field

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active
+        {
+            get;
+            set;
+        }
+        public DateTime DateAdded { get; set; }
+        public string StaffNo { get; set; }
+        public string StaffFullName { get; set; }
+        public bool StaffGender { get; set; }
+        public string CustomerNo { get; set; }
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -14,7 +14,73 @@ namespace Testing1
             clsStaff AnStaff = new clsStaff();
             //test to see that it exists
             Assert.IsNotNull(AnStaff);
+        }
+        [TestMethod]
+        public void StaffFullNameOK()
+        {
+            //Testing Staff Full Name property
+            clsStaff AnStaff = new clsStaff();
+            string TestData = "Mherab Ahmed";
+            AnStaff.StaffFullName = TestData;
+            Assert.AreEqual(AnStaff.StaffFullName, TestData);
+
 
         }
+
+        [TestMethod]
+        public void StaffDOBOK()
+        {
+            //Testing Staff DOB property
+            clsStaff AnStaff = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            AnStaff.DateAdded = TestData;
+            Assert.AreEqual(AnStaff.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void StaffNoOK()
+        {
+            //Testing Staff No property
+            clsStaff AnStaff = new clsStaff();
+            string TestData = "21";
+            AnStaff.StaffNo = TestData;
+            Assert.AreEqual(AnStaff.StaffNo, TestData);
+
+        }
+        [TestMethod]
+        public void StaffGenderOK()
+        {
+            //Testing Staff Gender property
+            clsStaff AnStaff = new clsStaff();
+            Boolean TestData = true;
+            AnStaff.StaffGender = TestData;
+            Assert.AreEqual(AnStaff.StaffGender, TestData);
+        }
+        [TestMethod]
+        public void CustomerNoOK()
+        {
+            //Testing Customer No property
+            clsStaff AnStaff = new clsStaff();
+            string TestData = "16";
+            AnStaff.CustomerNo = TestData;
+            Assert.AreEqual(AnStaff.CustomerNo, TestData);
+
+        }
+        [TestMethod]
+        public void StaffActivityOK()
+        {
+            //Testing Staff Activity
+            clsStaff AnStaff = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            AnStaff.DateAdded = TestData;
+            Assert.AreEqual(AnStaff.DateAdded, TestData);
+
+        }
+
+
+
+
+
+
+
     }
 }
