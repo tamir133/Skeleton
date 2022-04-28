6e7dabf6222d99b9ad35aa9895025489de02f2c2
Mherab Ahmed <P2598656@my365.dmu.ac.uk>
2022-04-28 14:44:54 +0100

    added simple presentation layer

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStaff AnStaff = new clsStaff();
+        AnStaff = (clsStaff) Session["AnStaff"];
+        Response.Write(AnStaff.StaffNo);
+        Response.Write(AnStaff.StaffFullName);
+        Response.Write(AnStaff.StaffGender);
+        Response.Write(AnStaff.StaffDOB);
+        Response.Write(AnStaff.StaffActivity);
 
     }
 }
