71997114b2ded135a3e26b901cf49b51f5b25ca5
Nirmal Vinodchandra <P2595193@my365.dmu.ac.uk>
2022-05-04 17:03:23 +0100

    Test Method are create for each field

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public string CustomerFullName { get; set; }
+        public int CustomerId { get; set; }
+        public bool CustomerGender { get; set; }
+        public DateTime CustomerDateOfBirth { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAddress { get; set; }
     }
 }
