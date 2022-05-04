f5acd2c20f4d4563a5805c9939faa30f3f1e50ee
Nirmal Vinodchandra <P2595193@my365.dmu.ac.uk>
2022-05-05 00:50:35 +0100

    Find method for all field are created and hardcoded the results in the find property.

--- Testing3/tstCustomer.cs
+++ Testing3/tstCustomer.cs
@@ -77,5 +77,125 @@ namespace Testing3
             ACustomer.CustomerAddress = TestData;
             Assert.AreEqual(ACustomer.CustomerAddress, TestData);
         }
+
+        //====================FindMethod=================================
+
+        [TestMethod]
+
+        public void FindMethodOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            Assert.IsTrue(Found);
+        }
+
+        //====================CustomerIdFound==================
+
+        [TestMethod]
+
+        public void TestCustomerIdFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerId != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        //===============CustomerFullNameFound==================
+
+        [TestMethod]
+        public void TestCustomerFullNameFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerFullName != "Nirmal Vinodchandra")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        //========================CustomerGenderFound=======================
+
+        [TestMethod]
+        public void TestCustomerGenderFound()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerGender != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        //========================CustomerDateOfBirthFound=======================
+
+        [TestMethod]
+        public void TestCustomerDateOfBirthFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerDateOfBirth != Convert.ToDateTime("12/05/1998"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        //========================CustomerEmailFound=======================
+
+        [TestMethod]
+
+        public void TestCustomerEmailFound()
+
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerEmail != "nirmal@gmail.com")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        //========================CustomerAddressFound=======================
+
+        [TestMethod]
+        public void TestCustomerAddressFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+            if (ACustomer.CustomerAddress != "163 Some street")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
     }
 }
