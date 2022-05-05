12de64ad447ce9c01365f4ce7383da31b466209d
Nirmal Vinodchandra <P2595193@my365.dmu.ac.uk>
2022-05-06 00:53:14 +0100

    clsCustomercollection and tstCustomerCollection is added and code are filled and working.

---
+++ Testing3/tstCustomerCollection.cs
@@ -0,0 +1,70 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an istance of the class we want to create 
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.CustomerId = 1;
+            TestItem.CustomerFullName = "Nirmal Vinodchandra";
+            TestItem.CustomerGender = true;            
+            TestItem.CustomerDateOfBirth = Convert.ToDateTime("12/05/1998");
+            TestItem.CustomerEmail = "nirmal@gmail.com";
+            TestItem.CustomerAddress = "163 Some street";
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+        }
+
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            clsCustomer TestCustomer = new clsCustomer();
+            TestCustomer.CustomerId = 1;
+            TestCustomer.CustomerFullName = "Nirmal Vinodchandra";
+            TestCustomer.CustomerGender = true;
+            TestCustomer.CustomerDateOfBirth = Convert.ToDateTime("12/05/1998");
+            TestCustomer.CustomerEmail = "nirmal@gmail.com";
+            TestCustomer.CustomerAddress = "163 Some street";
+            AllCustomers.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.CustomerId = 1;
+            TestItem.CustomerFullName = "Nirmal Vinodchandra";
+            TestItem.CustomerGender = true;
+            TestItem.CustomerDateOfBirth = Convert.ToDateTime("12/05/1998");
+            TestItem.CustomerEmail = "nirmal@gmail.com";
+            TestItem.CustomerAddress = "163 Some street";
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+    }
+}
